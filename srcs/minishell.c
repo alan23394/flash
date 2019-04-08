8c5d4b494562e4d0eb16e6173b88cf030cff79ac
Alan Barnett <alanbarnett328@gmail.com>
2019-04-07 22:23:43 -0700

    feat(builtins/commands): implement commands
    Can now properly use the builtin and command functions to
    launch builtins and commands with their respective run_ functions.
    Currently run_command does nothing, but the functionality is
    there.

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,34 +6,58 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 13:46:05 by alan              #+#    #+#             */
-/*   Updated: 2019/04/07 16:22:12 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/07 18:11:52 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
 #include "minishell.h"
 #include "builtins.h"
+#include "command.h"
 #include "prompt.h"
 #include "ft_printf.h"
 #include "get_next_line.h"
 #include "ft_darr.h"
 
-int		main(int argc, char **argv)
+int		process_command(char *command)
+{
+	int	builtin_nr;
+	int	ret;
+
+	builtin_nr = check_builtins(command);
+	ret = 0;
+	if (builtin_nr == -1)
+		ret = run_command(command);
+	else
+	{
+		//ft_next_word
+		ret = run_builtin(builtin_nr, command);
+	}
+	return (ret);
+}
+
+int		print_error()
+{
+	ft_printfd(2, "%s\n", "get_next_line failed, I dunno but quit time now");
+	return (1);
+}
+
+int		main()
 {
 	char		*line;
-	int			len;
+	int			ret;
 	extern char	**environ;
 
-	(void)argc;
-	(void)argv;
 	g_environ = ft_dup_darr(environ);
 	print_prompt(1);
-	while (42)
+	while ((ret = get_next_line(0, &line)) != 0)
 	{
+		if (ret == -1)
+		{
+			return (print_error());
+		}
+		process_command(line);
 		print_prompt(1);
-		len = get_next_line(0, &line);
-		ft_printf("%d\n", len);
-		ft_printf("line: %s\n", line);
 	}
 	return (0);
 }
