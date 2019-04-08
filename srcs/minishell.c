e34d92f66debd2ec1d414da7af63f6e99a84d868
Alan Barnett <alanbarnett328@gmail.com>
2019-04-07 17:20:39 -0700

    chore(environment): new header with g_environ
    Make the environment variables global, and properly set them at the
    beginning of the shell.

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,11 +6,13 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 13:46:05 by alan              #+#    #+#             */
-/*   Updated: 2019/04/06 18:26:10 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/07 16:22:12 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "environment.h"
 #include "minishell.h"
+#include "builtins.h"
 #include "prompt.h"
 #include "ft_printf.h"
 #include "get_next_line.h"
@@ -21,12 +23,11 @@ int		main(int argc, char **argv)
 	char		*line;
 	int			len;
 	extern char	**environ;
-	char		**new_environ;
 
 	(void)argc;
 	(void)argv;
-	new_environ = ft_dup_darr(environ);
-	(void)new_environ;
+	g_environ = ft_dup_darr(environ);
+	print_prompt(1);
 	while (42)
 	{
 		print_prompt(1);
