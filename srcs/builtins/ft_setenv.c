aa886a3a0d2bc82618cc32b046dece4432fd761b
Alan Barnett <alanbarnett328@gmail.com>
2019-04-09 17:30:02 -0700

    chore(builtins): add setenv and unsetenv files
    Forgot to add these somehow, but they've been done for a bit.

---
+++ srcs/builtins/ft_setenv.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_setenv.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/07 16:25:52 by alan              #+#    #+#             */
+/*   Updated: 2019/04/08 05:20:19 by alan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "environment.h"
+#include "ft_word.h"
+#include "ft_printf.h"
+#include "ft_darr.h"
+
+int	ft_setenv(const char *command)
+{
+	int			ret;
+	char		**words;
+	int			i;
+
+	words = ft_wordsplit(command);
+	if (!words)
+	{
+		ft_printfd(2, "needs arguments dude\n");
+		return (1);
+	}
+	i = 0;
+	while (words[i])
+	{
+		ret = validate_env(words[i]);
+		if (ret == 1)
+			ft_printfd(2, "error \"%s\" bad\n", words[i]);
+		else
+		{
+			ret = add_env(words[i]);
+			// If we couldn't make more memory, don't hide the pointer
+			// If we successfully added the pointer, set words[i] to 0 so that
+			// way it doesn't get deleted
+			if (!ret)
+				ft_printfd(2, "error couldn't make new memory\n");
+			else
+				words[i] = 0;
+		}
+		++i;
+	}
+	ft_delete_darr((void **)words);
+	return (0);
+}
---
+++ srcs/builtins/ft_unsetenv.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_unsetenv.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/08 06:01:34 by alan              #+#    #+#             */
+/*   Updated: 2019/04/08 06:12:54 by alan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "environment.h"
+#include "ft_word.h"
+#include "ft_darr.h"
+#include "ft_printf.h"
+
+int	ft_unsetenv(const char *command)
+{
+	int			ret;
+	char		**words;
+	int			i;
+
+	words = ft_wordsplit(command);
+	if (!words)
+	{
+		ft_printfd(2, "needs arguments dude\n");
+		return (1);
+	}
+	i = 0;
+	while (words[i])
+	{
+		ret = validate_env_name(words[i]);
+		if (ret == 1)
+			ft_printfd(2, "error \"%s\" bad\n", words[i]);
+		else
+		{
+			ret = get_env_i(words[i]);
+			// If we couldn't make more memory, don't hide the pointer
+			// If we successfully added the pointer, set words[i] to 0 so that
+			// way it doesn't get deleted
+			if (ret == -1)
+				ft_printfd(2, "error no such environ\n");
+			else
+				ft_darrrm_i(&g_environ, ret);
+		}
+		++i;
+	}
+	ft_delete_darr((void **)words);
+	return (0);
+}
