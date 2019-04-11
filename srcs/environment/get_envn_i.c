03c92f9cc151eab7fdb082925960a9240560eb7c
Alan Barnett <alanbarnett328@gmail.com>
2019-04-11 06:58:13 -0700

    chore(environment): make function that takes namelen
    get_env_i uses ft_strlen to get the length, which means it assumes the string
    is null terminated, which usually means it's allocated as it's own thing,
    which is usually not the case. So I've made a new function that takes the
    length of the name to compare, so I can just use ft_strchr to find the =
    or something (or when validating the expansions, I can look for the first
    non-variable character).

---
+++ srcs/environment/get_envn_i.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_envn_i.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/09 22:31:46 by alan              #+#    #+#             */
+/*   Updated: 2019/04/09 22:32:22 by alan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "environment.h"
+#include "ft_string.h"
+
+/*
+** This function uses ft_strnequ with the env_namelen variable passed in,
+** which allows me to search with strings that aren't null terminated.
+** It returns -1 if the variable could not be found.
+*/
+
+int	get_envn_i(const char *env_name, int env_namelen)
+{
+	int	i;
+
+	i = 0;
+	while (g_environ[i])
+	{
+		if (ft_strnequ(env_name, g_environ[i], env_namelen))
+			return (i);
+		++i;
+	}
+	return (-1);
+}
