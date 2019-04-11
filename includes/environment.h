0d8f01a77cf5803dcac244f4ee50271fa426dc1a
Alan Barnett <alanbarnett328@gmail.com>
2019-04-11 07:04:55 -0700

    chore(get_envn_i): add declaration to header file

--- includes/environment.h
+++ includes/environment.h
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/06 21:10:35 by alan              #+#    #+#             */
-/*   Updated: 2019/04/09 02:38:47 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/11 03:25:12 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ const char	**g_environ;
 
 int			add_env(const char *env);
 int			get_env_i(const char *env_name);
+int			get_envn_i(const char *env_name, int env_namelen);
 const char	*get_env(const char *env_name);
 
 int			validate_env(const char *command);
