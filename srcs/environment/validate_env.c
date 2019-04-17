da4e318763812e7955da192e9421759ced3f11cb
Alan Barnett <alanbarnett328@gmail.com>
2019-04-17 02:41:13 -0700

    refactor(validate_env): change return value
    I've changed this function to return 1 on success, and 0 on failure. I
    have also made a function that validates an environment variable name
    with a length. This is so I can easily check if a variable name is valid
    or not when I have a certain amount of characters to check for

--- srcs/environment/validate_env.c
+++ srcs/environment/validate_env.c
@@ -1,32 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   verify_env.c                                       :+:      :+:    :+:   */
+/*   validate_env.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 03:39:01 by alan              #+#    #+#             */
-/*   Updated: 2019/04/08 03:43:53 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/15 04:41:30 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_utils.h"
 #include "ft_string.h"
 
+int	validate_envn_name(const char *env, int env_namelen)
+{
+	int	i;
+
+	i = 0;
+	if (env_namelen != 0 && !ft_isdigit(env[i]))
+	{
+		while (i < env_namelen)
+		{
+			if (!(ft_isalnum(env[i]) || env[i] == '_'))
+				return (0);
+			++i;
+		}
+		return (1);
+	}
+	return (0);
+}
+
 int	validate_env(const char *command)
 {
 	char	*sign;
 
 	sign = ft_strchr(command, '=');
-	if (sign && command != sign && !ft_isdigit(*command))
-	{
-		while (command < sign)
-		{
-			if (!ft_isalnum(*command) && *command != '_')
-				return (1);
-			++command;
-		}
-		return (0);
-	}
-	return (1);
+	if (sign)
+		return (validate_envn_name(command, sign - command));
+	return (0);
 }
