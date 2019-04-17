9486865351394f9c246751bbbd4c235208af9c4b
Alan Barnett <alanbarnett328@gmail.com>
2019-04-17 13:33:44 -0700

    refactor(validate_envn_name): move to new file

--- srcs/environment/validate_envn_name.c
+++ srcs/environment/validate_envn_name.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validate_env.c                                     :+:      :+:    :+:   */
+/*   validate_envn_name.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/08 03:39:01 by alan              #+#    #+#             */
-/*   Updated: 2019/04/17 11:23:34 by alan             ###   ########.fr       */
+/*   Created: 2019/04/17 11:22:36 by alan              #+#    #+#             */
+/*   Updated: 2019/04/17 11:23:12 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
-#include "ft_string.h"
 
-int	validate_env(const char *command)
+int	validate_envn_name(const char *env, int env_namelen)
 {
-	char	*sign;
+	int	len;
 
-	sign = ft_strchr(command, '=');
-	if (!sign)
-		return (0);
-	return (validate_envn_name(command, sign - command));
+	len = get_valid_envn_len(env, env_namelen);
+	if (len == env_namelen)
+		return (1);
+	return (0);
 }
