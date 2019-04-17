8a8bbb24c17795bbc719c96a49474f367015fb38
Alan Barnett <alanbarnett328@gmail.com>
2019-04-17 16:20:55 -0700

    refactor(validate_env_name): call validate_envn_name
    Instead of duplicating code, I'd rather call a strlen and use the other
    function I have set up for this.

--- srcs/environment/validate_env_name.c
+++ srcs/environment/validate_env_name.c
@@ -6,24 +6,20 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 06:05:58 by alan              #+#    #+#             */
-/*   Updated: 2019/04/08 06:15:38 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/17 16:19:27 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "environment.h"
 #include "ft_utils.h"
 #include "ft_string.h"
 
+/*
+** This function validates an entire string as an env name, by getting it's
+** length and then using validate_envn_name.
+*/
+
 int	validate_env_name(const char *command)
 {
-	if (!ft_isdigit(*command))
-	{
-		while (*command)
-		{
-			if (!ft_isalnum(*command) && *command != '_')
-				return (1);
-			++command;
-		}
-		return (0);
-	}
-	return (1);
+	return (validate_envn_name(command, ft_strlen(command)));
 }
