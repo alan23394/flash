616002ffd749bd4e6ca05435aac9887e38a5eb18
Alan Barnett <alanbarnett328@gmail.com>
2019-04-17 13:59:46 -0700

    chore(expansions.h): add new functions to header

--- includes/expansions.h
+++ includes/expansions.h
@@ -6,13 +6,21 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 08:10:48 by alan              #+#    #+#             */
-/*   Updated: 2019/04/11 08:11:10 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/17 11:37:16 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPANSIONS_H
 # define EXPANSIONS_H
 
+typedef struct s_list	t_list;
+
+int		get_chunk(t_list **list, char *chunk_start, char *chunk_end);
 char	*expand_command(char *command);
+
+int		expand_tilde(t_list **list, char *arg_start, char **chunk_start,
+			char *tilde_index);
+int		expand_dollar(t_list **list, char **chunk_start,
+			char *dollar_index);
 
 #endif
