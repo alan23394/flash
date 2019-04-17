462b587183202026edb18b571d70cedf730d3232
Alan Barnett <alanbarnett328@gmail.com>
2019-04-17 15:00:24 -0700

    chore(prompt): prompt selector jump table
    My prompt printing function now uses a jump table to print the correct
    prompt. It gets passed an index, validates it, and then runs the prompt
    function the index refers to. I changed the comments too, and removed
    the idea of a 4th prompt for now. So far it's going to be normal, single
    quote, double quote (1, 2, and 3). I might make a generic continuation
    prompt and be able to pass it the type of continuation, which I think
    would be nice. I fixed prompts 2 and 3, they didn't have a comma after
    the format string and were printing really strange stuff.

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 16:25:45 by alan              #+#    #+#             */
-/*   Updated: 2019/04/06 18:31:17 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/17 14:58:49 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@
 ** Printing a prompt must be done through a call to print_prompt, which indexes
 ** a jump table with the sent value
 ** Which prompt to print gets decided when certain things happen, for example:
-**	No closing quote/parenthesis gets prompt 2,
-**	No newline (for ex when ending line with \) gets prompt 3,
-**	Not a finished script (if [ -f file ]; then [newline]) gets prompt 4
+**	No closing single quote gets prompt 2,
+**	No closing double quote gets prompt 3
 ** Those extra prompts/rules are not implemented yet, so for now it's just
 ** prompt 1, which happens for the start of every command.
-** They are also subject to change ofc, that's just the plan for now
+** They are also subject to change, that's just the plan for now
 */
 
 static void	prompt_1()
@@ -37,15 +36,32 @@ static void	prompt_1()
 
 static void	prompt_2()
 {
-	ft_printf("%s> %s"
+	ft_printf("%s\'> %s",
 			DARK_MAGENTA,
 			NORMAL);
 }
 
-void		print_prompt(int prompt)
+static void	prompt_3()
 {
-	if (prompt == 1)
-		prompt_1();
-	else if (prompt == 2)
-		prompt_2();
+	ft_printf("%s\"> %s",
+			DARK_MAGENTA,
+			NORMAL);
+}
+
+/*
+** Uses a jump table to run a prompt.
+** This also checks to make sure prompt_index will actually fit in the array.
+** Index - 1 is used because arrays start at 0, but these things start at 1.
+*/
+
+void		print_prompt(int prompt_index)
+{
+	static void	(*run_prompt[3])() = {
+		prompt_1,
+		prompt_2,
+		prompt_3,
+	};
+
+	if (prompt_index >= 1 && prompt_index <= 3)
+		run_prompt[prompt_index - 1]();
 }
