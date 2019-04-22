b99cb064ce99ddf04670bc749fbce3b9237ab513
Alan Barnett <alanbarnett328@gmail.com>
2019-04-22 06:02:01 -0700

    refactor(main): arg list
    Receive and pass around an argument linked list in the main, instead
    of a double array or string

--- srcs/main.c
+++ srcs/main.c
@@ -12,36 +12,33 @@
 
 #include "environment.h"
 #include "expansions.h"
-#include "minishell.h"
 #include "builtins.h"
 #include "command.h"
 #include "prompt.h"
 #include "get_next_line.h"
 #include "ft_darr.h"
 #include "ft_word.h"
+#include "ft_list.h"
 #include "error.h"
 
-int		process_command(const char *command)
+int		process_command(t_list *args)
 {
 	int	builtin_nr;
 	int	ret;
 
-	builtin_nr = check_builtins(command);
+	builtin_nr = check_builtins(args->content);
 	ret = 0;
 	if (builtin_nr == -1)
-		ret = run_command(command);
+		ret = run_command(args);
 	else
-	{
-		command = ft_wordnext(command);
-		ret = run_builtin(builtin_nr, command);
-	}
+		ret = run_builtin(builtin_nr, args);
 	return (ret);
 }
 
 int		main()
 {
 	char		*line;
-	char		*expanded;
+	t_list		*args;
 	int			ret;
 	extern char	**environ;
 
@@ -54,8 +51,8 @@ int		main()
 		{
 			return (print_error("get_next_line: couldn't make memory"));
 		}
-		expanded = expand_command(line);
-		process_command(expanded);
+		args = expand_command(line);
+		process_command(args);
 		print_prompt(1);
 	}
 	return (0);
