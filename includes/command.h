68a35d1f9dc2c7d71eb44f39781debeeebe9d545
Alan Barnett <alanbarnett328@gmail.com>
2019-04-22 06:20:01 -0700

    chore(conv): test convert list to darr

--- includes/command.h
+++ includes/command.h
@@ -6,13 +6,15 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 17:33:50 by alan              #+#    #+#             */
-/*   Updated: 2019/04/07 23:07:47 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/22 05:01:46 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMAND_H
 # define COMMAND_H
 
-int	run_command(const char *command);
+typedef struct s_list	t_list;
+
+int	run_command(t_list *args);
 
 #endif
--- srcs/command.c
+++ srcs/command.c
@@ -6,11 +6,14 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 17:33:00 by alan              #+#    #+#             */
-/*   Updated: 2019/04/15 00:41:00 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/22 05:21:35 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "ft_list.h"
+#include "ft_darr.h"
+#include "ft_conv.h"
 #include <unistd.h>
 
 /*
@@ -18,8 +21,11 @@
 ** The command is verified to not be a builtin when using these
 */
 
-int	run_command(const char *command)
+int	run_command(t_list *args)
 {
-	ft_printf("command: %s\n", command);
+	const char	**d_args;
+
+	d_args = ft_lst_to_darr(args);
+	ft_print_darr(d_args);
 	return (0);
 }
