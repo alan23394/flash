ab065bb48d0145e6f8954edd64bc0aa0b9c1217d
Alan Barnett <alanbarnett328@gmail.com>
2019-04-23 03:56:17 -0700

    chore(path): make new string with path
    combine path string and new command into one string

--- srcs/command.c
+++ srcs/command.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 17:33:00 by alan              #+#    #+#             */
-/*   Updated: 2019/04/22 05:21:35 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/23 03:54:32 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include "ft_list.h"
 #include "ft_darr.h"
 #include "ft_conv.h"
+#include "ft_string.h"
+#include "environment.h"
 #include <unistd.h>
 
 /*
@@ -21,11 +23,61 @@
 ** The command is verified to not be a builtin when using these
 */
 
+/*
+** This function makes a new string, bounded by start and end inclusive, with
+** append_string added after a /. It is used to combine a path string from the
+** PATH variable and a command into one string with the path of a variable.
+*/
+
+static char	*get_path_chunk_app(const char *chunk_start, char *chunk_end,
+				const char *append_string)
+{
+	int		chunk_len;
+	int		app_len;
+	char	*new_string;
+
+	chunk_len = (chunk_end - chunk_start) + 1;
+	if (chunk_len <= 0)
+		return (0);
+	// TODO fix strlen segfault when unsafe
+	app_len = ft_strlen(append_string);
+	new_string = ft_strnew(chunk_len + 1 + app_len);
+	ft_strncpy(new_string, chunk_start, chunk_len);
+	new_string[chunk_len] = '/';
+	ft_strncpy(new_string + chunk_len + 1, append_string, app_len);
+	return (new_string);
+}
+
+/*
+** TODO: be able to turn on and off path caching (caching in general)
+*/
+
+int		get_paths(const char *command)
+{
+	const char	*path;
+	char		*new_command;
+
+	path = get_envn("PATH", 4);
+	if (!path)
+		return (0);
+	new_command = get_path_chunk_app(path, ft_strchr(path, ':') - 1, command);
+	ft_printf("%s\n", new_command);
+	return (0);
+	/*
+	while (*path)
+	{
+		
+	}
+	*/
+}
+
 int	run_command(t_list *args)
 {
 	const char	**d_args;
+	int			something;
 
 	d_args = ft_lst_to_darr(args);
 	ft_print_darr(d_args);
-	return (0);
+	something = get_paths((const char *)args->content);
+	return (something);
 }
