9764581a71eeabf9d3684efbd1ef652185162d75
Alan Barnett <alanbarnett328@gmail.com>
2019-04-23 05:05:27 -0700

    chore(path): finds executable
    Correctly finds the executable in the path. It prints if the file
    isn't executable, and stops looking also (instead of checking in
    other folders).

--- srcs/command.c
+++ srcs/command.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 17:33:00 by alan              #+#    #+#             */
-/*   Updated: 2019/04/23 03:54:32 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/23 05:00:48 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include "ft_string.h"
 #include "environment.h"
 #include <unistd.h>
+#include <sys/stat.h>
 
 /*
 ** This file will be the controller for running a command
@@ -29,7 +30,7 @@
 ** PATH variable and a command into one string with the path of a variable.
 */
 
-static char	*get_path_chunk_app(const char *chunk_start, char *chunk_end,
+static char	*get_path_chunk_app(const char *chunk_start, const char *chunk_end,
 				const char *append_string)
 {
 	int		chunk_len;
@@ -52,32 +53,48 @@ static char	*get_path_chunk_app(const char *chunk_start, char *chunk_end,
 ** TODO: be able to turn on and off path caching (caching in general)
 */
 
-int		get_paths(const char *command)
+char	*get_paths(const char *command)
 {
 	const char	*path;
 	char		*new_command;
+	struct stat	stats;
+	int			len;
 
 	path = get_envn("PATH", 4);
 	if (!path)
 		return (0);
-	new_command = get_path_chunk_app(path, ft_strchr(path, ':') - 1, command);
-	ft_printf("%s\n", new_command);
-	return (0);
-	/*
 	while (*path)
 	{
-		
+		len = ft_strchr_end(path, ':') - path;
+		new_command = get_path_chunk_app(path, path + len - 1, command);
+		if (!new_command)
+			return (0);
+		if (stat(new_command, &stats) == 0)
+		{
+			if (!(stats.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)))
+			{
+				ft_printf("error: no permissions on %s\n", new_command);
+				return (0);
+			}
+			else
+			{
+				return (new_command);
+			}
+		}
+		path += len + !!path[len];
+		ft_strdel(&new_command);
 	}
-	*/
+	return (0);
 }
 
 int	run_command(t_list *args)
 {
 	const char	**d_args;
-	int			something;
+	char		*something;
 
 	d_args = ft_lst_to_darr(args);
 	ft_print_darr(d_args);
 	something = get_paths((const char *)args->content);
-	return (something);
+	ft_printf("%s\n", something);
+	return (0);
 }
