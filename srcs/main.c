0d5c9cc56db932842a93907b8a9f8e57d797734c
Alan Barnett <alanbarnett328@gmail.com>
2019-04-24 00:29:00 -0700

    refactor(main): update main with other changes
    Moved function gets taken out, and I updated my print error command so I must
    change it here also.

--- srcs/main.c
+++ srcs/main.c
@@ -6,34 +6,18 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 13:46:05 by alan              #+#    #+#             */
-/*   Updated: 2019/04/22 05:13:07 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/24 00:20:34 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
 #include "expansions.h"
-#include "builtins.h"
 #include "command.h"
 #include "prompt.h"
 #include "get_next_line.h"
 #include "ft_darr.h"
-#include "ft_word.h"
 #include "ft_list.h"
 #include "error.h"
-
-int		process_command(t_list *args)
-{
-	int	builtin_nr;
-	int	ret;
-
-	builtin_nr = check_builtins(args->content);
-	ret = 0;
-	if (builtin_nr == -1)
-		ret = run_command(args);
-	else
-		ret = run_builtin(builtin_nr, args);
-	return (ret);
-}
 
 int		main()
 {
@@ -49,7 +33,7 @@ int		main()
 	{
 		if (ret == -1)
 		{
-			return (print_error("get_next_line: couldn't make memory"));
+			return (print_error("get_next_line", "couldn't make memory"));
 		}
 		args = expand_command(line);
 		process_command(args);
