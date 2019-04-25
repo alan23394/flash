e4976f8b8d33aa956b53bb2938a8543a8543006e
Alan Barnett <alanbarnett328@gmail.com>
2019-04-25 12:16:56 -0700

    feat(exit): add exit command
    Reorganize some of the main file, break out the main loop into a new function
    to clean up some stuff, and give me space for the exit checking and appropriate
    error handling. I had to set ret = 0 at the end of the shell_loop function,
    because in the case that we're continuing a line, ret doesn't get reset at
    the gnl call (because it doesn't make it that far in the while condition)
    and so if for some reason it was set to -1 by process_command, it would
    trigger the get_next_line error message.

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 13:46:05 by alan              #+#    #+#             */
-/*   Updated: 2019/04/24 08:08:37 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/25 12:03:56 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,47 @@
 #include "expansions.h"
 #include "command.h"
 #include "prompt.h"
+#include "ft_string.h"
 #include "get_next_line.h"
 #include "ft_darr.h"
 #include "ft_list.h"
 #include "error.h"
 
-int		main()
+void	shell_loop()
 {
 	char		*line;
 	t_list		*args;
 	int			ret;
-	extern char	**environ;
 
 	line = 0;
-	g_environ = ft_dup_darr((const char **)environ);
-	// Increase SHLVL variable
-	print_prompt(1);
 	while ((line && *line) || (ret = get_next_line(0, &line)) != 0)
 	{
 		if (ret == -1)
 		{
-			return (print_error("get_next_line", "couldn't make memory"));
+			(void)print_error("get_next_line", "couldn't make memory");
+			break ;
 		}
 		args = expand_command(&line);
+		if (args && ft_strequ(args->content, "exit"))
+		{
+			ft_lstdel(&args, ft_lstmemdel);
+			break ;
+		}
 		ret = process_command(args);
+		ft_lstdel(&args, ft_lstmemdel);
+		ret = 0;
 		if (*line == '\0')
 			print_prompt(1);
-		ft_lstdel(&args, ft_lstmemdel);
 	}
+}
+
+int		main()
+{
+	extern char	**environ;
+
+	g_environ = ft_dup_darr((const char **)environ);
+	// Increase SHLVL variable
+	print_prompt(1);
+	shell_loop();
 	return (0);
 }
