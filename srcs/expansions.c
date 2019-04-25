fa746bb3e560815a4c7ddce5c7c3d8046bd5320b
Alan Barnett <alanbarnett328@gmail.com>
2019-04-25 10:14:11 -0700

    fix(remove strlens): make expanding nicer
    I don't need to call strlen for content_size, I can just send a pointer to
    the function that creates the arguments and take back it's length. Yay!

--- srcs/expansions.c
+++ srcs/expansions.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 08:02:16 by alan              #+#    #+#             */
-/*   Updated: 2019/04/25 07:03:10 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/25 10:13:15 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,14 @@
 #include "ft_printf.h"
 #include "ft_list.h"
 
-static char	*expand_arg(char **command)
+/*
+** This function takes a command at the start of an argument, and splits that
+** argument into the expanded chunks (replacing ~ for the home path, $VARs
+** etc), and then combines that list into one new string, which it returns. It
+** sets the len pointer passed in to the length of the string it returns.
+*/
+
+static char	*expand_arg(char **command, int *len)
 {
 	t_list	*arg_list;
 	size_t	arg_len;
@@ -38,6 +45,7 @@ static char	*expand_arg(char **command)
 	if (chunk_start != *command)
 		arg_len += get_chunk(&arg_list, chunk_start, *command - 1);
 	arg = ft_lstcomb_str(arg_list, arg_len);
+	*len = arg_len;
 	ft_lstdel(&arg_list, 0);
 	return (arg);
 }
@@ -46,8 +54,10 @@ t_list		*expand_command(char **command)
 {
 	t_list	*list;
 	char	*new_command;
+	int		arg_len;
 
 	list = 0;
+	arg_len = 0;
 	if (!*command)
 		return (0);
 	while (**command && **command != ';')
@@ -55,10 +65,9 @@ t_list		*expand_command(char **command)
 		*command = (char *)ft_skipspace(*command);
 		if (**command && **command != ';')
 		{
-			new_command = expand_arg(command);
+			new_command = expand_arg(command, &arg_len);
 			if (new_command)
-				ft_lstadd_tail(&list,
-						ft_lstinit(new_command, ft_strlen(new_command)));
+				ft_lstadd_tail(&list, ft_lstinit(new_command, arg_len));
 		}
 	}
 	if (**command && **command == ';')
