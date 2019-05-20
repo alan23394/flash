ac6643163382dbf2cd6d9cee3f6d2c62df4303f6
Alan Barnett <alanbarnett328@gmail.com>
2019-05-20 03:23:03 -0700

    feat(shlvl): increase shlvl
    This commit adds an env_inc and envn_inc function, which increment the integer
    variable inside an environment variable. It's used to increment the shell level
    variable. Also this commit adds a void shell_setup function, which does things
    like incrementing shlvl, setting the shell variable, and printing the prompt
    to start with. the env_inc functions need to be moved still.
    
    squash fix(shell var): don't change
    
    The SHELL variable, I found out, actually represents the login shell
    of the user, so new terminals made use that shell. It does not represent
    the currently running shell, so I should not change it.

--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,19 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 13:46:05 by alan              #+#    #+#             */
-/*   Updated: 2019/04/28 04:51:24 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/05/31 07:28:42 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
 #include "expansions.h"
 #include "command.h"
+#include "environment.h"
 #include "prompt.h"
+#include "ft_utils.h"
 #include "ft_string.h"
 #include "get_next_line.h"
+#include "ft_printf.h"
 #include "ft_darr.h"
 #include "ft_list.h"
 #include "error.h"
@@ -52,12 +55,75 @@ static int	shell_loop()
 	return (0);
 }
 
-int			main()
+/*
+** This function attempts to increment the integer value inside an env
+** It takes the env to increment, and the length of it's name
+** If the variable doesn't exist in the environment, it is set to 1.
+**
+** What we need to do
+** find if the variable exists
+** if it isn't in there,
+**	make a string with 1
+**	add it
+** if it is,
+**	make sure it's a number
+**	if it isn't,
+**		error message, and quit
+**	get it's int value
+**	make a new string with increased int value
+**	delete the old string, and replace it with the new one
+**		(this skips the additional validation required by other functions)
+*/
+
+void	envn_inc(char *env, int env_namelen)
+{
+	int			env_index;
+	int			env_num;
+	const char	*new_env;
+	const char	*env_val;
+
+	if (!env || !env_namelen)
+		return ;
+	env_index = get_envn_index(env, env_namelen);
+	if (env_index == -1)
+	{
+		new_env = make_envn_int(env, env_namelen, 1);
+		ft_darradd(&g_environ, new_env);
+	}
+	else
+	{
+		env_val = get_envi_value(env_index);
+		if (!ft_str_is_num(env_val))
+		{
+			print_error("error: expected integer", env_val);
+			return ;
+		}
+		env_num = get_envi_int(env_index);
+		new_env = make_envn_int(env, env_namelen, env_num + 1);
+		ft_strdel((char **)&g_environ[env_index]);
+		g_environ[env_index] = new_env;
+	}
+}
+
+void	env_inc(char *env_name)
+{
+	if (!env_name)
+		return ;
+	envn_inc(env_name, ft_strlen(env_name));
+}
+
+void	shell_setup(void)
+{
+	env_inc("SHLVL");
+	print_prompt(1);
+}
+
+int		main(void)
 {
 	extern char	**environ;
 
 	g_environ = ft_dup_darr((const char **)environ);
-	// Increase SHLVL variable
-	print_prompt(1);
-	return (shell_loop());
+	shell_setup();
+	shell_loop();
+	return (0);
 }
