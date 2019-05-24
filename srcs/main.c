3afafa1c3085b17b40dba60e3fae533923fd50b5
Alan Barnett <alanbarnett328@gmail.com>
2019-05-24 00:51:03 -0700

    refactor(shell_loop): return status
    Make the shell_loop function return the exit status of the shell.
    
    fix(deletion bug): reset line cursor
    
    When freeing the line string, I never reset the cursor to 0, so it
    had undefined behavior when pointing at freed memory.

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 13:46:05 by alan              #+#    #+#             */
-/*   Updated: 2019/04/25 12:03:56 by alan             ###   ########.fr       */
+/*   Updated: 2019/04/28 04:51:24 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,44 @@
 #include "ft_list.h"
 #include "error.h"
 
-void	shell_loop()
+static int	shell_loop()
 {
 	char		*line;
-	t_list		*args;
+	char		*line_start;
 	int			ret;
+	t_list		*args;
 
 	line = 0;
-	while ((line && *line) || (ret = get_next_line(0, &line)) != 0)
+	ret = 0;
+	args = 0;
+	while ((line && *line) || (((ret = get_next_line(0, &line_start)) != 0) && (line = line_start)))
 	{
 		if (ret == -1)
-		{
-			(void)print_error("get_next_line", "couldn't make memory");
-			break ;
-		}
+			return (print_error("get_next_line", "couldn't make memory"));
 		args = expand_command(&line);
 		if (args && ft_strequ(args->content, "exit"))
-		{
-			ft_lstdel(&args, ft_lstmemdel);
 			break ;
-		}
 		ret = process_command(args);
 		ft_lstdel(&args, ft_lstmemdel);
-		ret = 0;
 		if (*line == '\0')
+		{
+			ft_strdel(&line_start);
+			line = 0;
 			print_prompt(1);
+		}
+		ret = 0;
 	}
+	ft_lstdel(&args, ft_lstmemdel);
+	ft_strdel(&line_start);
+	return (0);
 }
 
-int		main()
+int			main()
 {
 	extern char	**environ;
 
 	g_environ = ft_dup_darr((const char **)environ);
 	// Increase SHLVL variable
 	print_prompt(1);
-	shell_loop();
-	return (0);
+	return (shell_loop());
 }
