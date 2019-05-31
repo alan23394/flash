be60d8024a8b84ee453513ad02e537a2475962cb
Alan Barnett <alanbarnett328@gmail.com>
2019-05-31 09:23:55 -0700

    chore(clean main): remove extra includes, tidying
    Remove some extra includes now that I've moved the functions that needed
    them out of the file, and also change some variable names to make more
    sense (line changed to cur, line_start changed to line).

--- srcs/main.c
+++ srcs/main.c
@@ -6,52 +6,50 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 13:46:05 by alan              #+#    #+#             */
-/*   Updated: 2019/05/29 03:42:29 by alan             ###   ########.fr       */
+/*   Updated: 2019/05/31 09:17:55 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environment.h"
 #include "expansions.h"
 #include "command.h"
-#include "environment.h"
 #include "prompt.h"
-#include "ft_utils.h"
 #include "ft_string.h"
 #include "get_next_line.h"
-#include "ft_printf.h"
 #include "ft_darr.h"
 #include "ft_list.h"
 #include "error.h"
 
-static int	shell_loop()
+static int	shell_loop(void)
 {
+	char		*cur;
 	char		*line;
-	char		*line_start;
 	int			ret;
 	t_list		*args;
 
-	line = 0;
+	cur = 0;
 	ret = 0;
 	args = 0;
-	while ((line && *line) || (((ret = get_next_line(0, &line_start)) != 0) && (line = line_start)))
+	while ((cur && *cur) ||
+			(((ret = get_next_line(0, &line)) != 0) && (cur = line)))
 	{
 		if (ret == -1)
 			return (print_error("get_next_line", "couldn't make memory"));
-		args = expand_command(&line);
+		args = expand_command(&cur);
 		if (args && ft_strequ(args->content, "exit"))
 			break ;
 		ret = process_command(args);
 		ft_lstdel(&args, ft_lstmemdel);
-		if (*line == '\0')
+		if (*cur == '\0')
 		{
-			ft_strdel(&line_start);
-			line = 0;
+			ft_strdel(&line);
+			cur = 0;
 			print_prompt(1);
 		}
 		ret = 0;
 	}
 	ft_lstdel(&args, ft_lstmemdel);
-	ft_strdel(&line_start);
+	ft_strdel(&line);
 	return (0);
 }
 
